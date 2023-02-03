f58033bb0831d5eb2a2aa10a6f3b7301fdfdfa18
Jerebtw <jeremialp41@gmail.com>
2023-02-03 18:16:35 +0100

    Move to swc

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+  },
   preset: "ts-jest",
   testEnvironment: "node",
 };
